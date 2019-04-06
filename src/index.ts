9691f4f25e243bd7ed32d1209270bc3039f7987e
lwyj123 <liang.pearce@gmail.com>
2019-04-06 14:06:18 +0800

    fix: 修复types的bug

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
  * @author Yuyi Liang <liang.pearce@gmail.com>
  */
 
-import { Player } from './core/player';
+import { Player } from './core/yuefu';
 
 // tslint:disable-next-line:prefer-type-cast
 (window as any).Yuefu = Player;
